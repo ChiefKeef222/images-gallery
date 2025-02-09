8d8b8b38732ae9c0bdcd18223c1311976425fd44
Arhat Amanzhol <aarhat144@gmail.com>
2025-02-09 23:20:26 +0500

    controlled search component

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header.js';
-
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.js";
+import Search from "./components/Search.js";
 
 function App() {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
+  console.log(word)
+
   return (
     <div>
-      <Header title = "Images Gallery"/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
+const navbarStyle = {
+    backgroundColor: '#70A1FF'
+};
+
 
 const Header = ({ title }) => {
     return (
-    <Navbar bg="light" variant="light">
+    <Navbar style={navbarStyle} variant="light">
         <Container>
             <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Container>
