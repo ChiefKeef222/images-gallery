08ebe8a5cb134ba94ebcbb3602ddcb1742b9a3f2
Arhat Amanzhol <aarhat144@gmail.com>
2025-02-09 19:30:30 +0500

    Initial header component

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header.js';
+
+
 function App() {
   return (
-    <div className="App">
-     <h1>Images Gallery</h1>
+    <div>
+      <Header title = "Images Gallery"/>
     </div>
   );
 }
---
+++ front/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+
+const Header = ({ title }) => {
+    return (
+    <Navbar bg="light" variant="light">
+        <Container>
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
+        </Container>
+    </Navbar>
+
+    );
+};
+
+export default Header;
