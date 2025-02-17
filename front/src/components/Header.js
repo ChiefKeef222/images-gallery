66f08b2b63ed871caab890f4704244ea49ea5993
Arhat Amanzhol <aarhat144@gmail.com>
2025-02-17 22:06:22 +0500

    added header new logo

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: '#70A1FF',
+  backgroundColor: '#0b1d27',
 };
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '12rem', maxHeight: '3rem' }} />
       </Container>
     </Navbar>
   );
